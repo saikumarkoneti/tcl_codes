2f544d248928e552bec6b4b99caa27a50f9eee5c
saikumarkoneti <112846846+saikumarkoneti@users.noreply.github.com>
2024-09-23 10:58:57 +0530

    Create finding_files.tcl

---
+++ finding_files.tcl
@@ -0,0 +1,17 @@
+#set a 90
+#unset a
+#if {![info exists a]} {
+#	puts "ledhu"
+#} else {
+#	puts "undhi"
+#}
+#set listofnets [getallnets "some_pattern"]
+set color "red dark yelow green blue purple"
+foreach col $color {
+	puts $col
+}
+#hgd
+set filename [glob *tcl]
+foreach names $filename {
+puts "filename=$names"
+}
